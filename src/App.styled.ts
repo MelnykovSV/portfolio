9be48222c52d132387aad87d08c0469aa649e4d6
MelnykovSV <svmelnykov@gmail.com>
2024-03-25 01:50:34 +0200

    hidden canvas on mobile, added backup background color

--- src/App.styled.ts
+++ src/App.styled.ts
@@ -2,9 +2,10 @@ import styled from '@emotion/styled';
 import bgImage from './assets/space.jpg';
 
 export const Wrapper = styled.div`
-  background-image: url(${bgImage});
+  background-image: url(${bgImage}), linear-gradient(to right, #0a192f, #0a192f);
   background-size: cover;
   background-repeat: no-repeat;
+  background-position: center;
   width: 100vw;
   height: 100vh;
   position: relative;
@@ -14,6 +15,12 @@ export const Canvas = styled.canvas`
   width: 100%;
   height: 100%;
   display: block;
+
+  visibility: hidden;
+
+  @media screen and (min-width: 768px) {
+    visibility: visible;
+  }
 `;
 
 export const Container = styled.div`
@@ -24,8 +31,6 @@ export const Container = styled.div`
   left: 0;
   z-index: 10;
   overflow: auto;
-
-  
 
   h1 {
     padding-left: 30px;
