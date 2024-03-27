f98ca0120a6d355a0059a7aada63ecb30c5a2798
MelnykovSV <svmelnykov@gmail.com>
2024-03-27 19:34:07 +0200

    wrote info section

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,34 @@
 export default function MainPage() {
   return (
-    <div>
-      <h1>Main page</h1>
-    </div>
+    <>
+      <section>
+        <h1>Serhii Melnykov</h1>
+        <h2> Full-stack Web Developer</h2>
+        <p>Making ideas come alive on the web</p>
+        <p>
+          As a goal-oriented and passionate front-end developer, I thrive on challenges, seeing each
+          project as an opportunity for growth and innovation. My dedication to delivering
+          exceptional results drives me to constantly seek out new and better ways to craft seamless
+          user experiences with clean, efficient, and scalable code, using the best practices.
+        </p>
+        <p>
+          I believe that work is a continuous journey of learning and improvement, and I am
+          committed to expanding my knowledge and expertise to stay ahead in the fast-paced world of
+          web development. With a collaborative mindset and an insatiable eagerness to learn, I am
+          fully prepared to dedicate myself to creating remarkable websites and applications,
+          contributing as a valuable asset to any team.
+        </p>
+      </section>
+      <section>
+        <h2>My Skills</h2>
+      </section>
+
+      <section>
+        <h2>My Projects</h2>
+      </section>
+      <section>
+        <h2>Get In Touch</h2>
+      </section>
+    </>
   );
 }
